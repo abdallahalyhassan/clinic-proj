1ea340d733adcd15aaebc75a5e1d54f314762812
abdallah aly <154508942+abdallahalyhassan@users.noreply.github.com>
2025-03-28 03:25:03 +0200

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\doctor;
+use App\Models\oppintement;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+      $this->call([
+        User::class,
+        majorseeder::class,
+        doctor::class,
+        oppintement::class,
+
+      ]);
     }
 }
